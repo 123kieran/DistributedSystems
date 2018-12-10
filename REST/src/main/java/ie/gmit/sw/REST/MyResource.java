d1da8a8280cc4c03db29a5d3610e3c233acacbea
123kieran <123kieran@gmail.com>
2018-12-10 17:36:50 +0000

    added read write update and delete to resources

--- REST/src/main/java/ie/gmit/sw/REST/MyResource.java
+++ REST/src/main/java/ie/gmit/sw/REST/MyResource.java
@@ -1,25 +1,70 @@
 package ie.gmit.sw.REST;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import ie.gmit.sw.ds.Setup.Interface;
+import ie.gmit.sw.ds.Models.Booking;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
 @Path("myresource")
 public class MyResource {
-
-    /**
+     /**
      * Method handling HTTP GET requests. The returned object will be sent
      * to the client as "text/plain" media type.
      *
      * @return String that will be returned as a text/plain response.
+     * @throws Exception 
+     * @throws RemoteException 
+     * @throws MalformedURLException 
+     * @throws SQLException 
      */
+	@Path("/readCarHire")
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Booking> getIt() throws MalformedURLException, RemoteException, Exception, SQLException {
+    	Interface CarBookingService;
+    	CarBookingService = (Interface) Naming.lookup("rmi://127.0.0.1:1099/database");
+    	
+        return CarBookingService.readCarHire();
     }
+	
+	@Path("/writeCarHire")
+	@POST
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Booking> writeIt(String input) throws MalformedURLException, RemoteException, Exception, SQLException {
+		Interface CarBookingService;
+    	CarBookingService = (Interface) Naming.lookup("rmi://127.0.0.1:1099/database");
+        return CarBookingService.writeCarHire(input);
+    }
+	
+	@Path("/updateCarHire")
+	@POST
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Booking> updateIt(String input) throws MalformedURLException, RemoteException, Exception, SQLException {
+		Interface CarBookingService;
+    	CarBookingService = (Interface) Naming.lookup("rmi://127.0.0.1:1099/database");
+        return CarBookingService.updateCarHire(input);
+    }
+	
+	@Path("/deleteCarHire")
+	@POST
+    @Produces(MediaType.APPLICATION_XML)
+    public List<Booking> deleteIt(String input) throws MalformedURLException, RemoteException, Exception, SQLException {
+		Interface CarBookingService;
+    	CarBookingService = (Interface) Naming.lookup("rmi://127.0.0.1:1099/database");
+        return CarBookingService.deleteCarHire(input);
+    }
+	
 }
