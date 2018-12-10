f9d6c8e6af330a82a5cf38acdfb8c1d1416f69c8
123kieran <123kieran@gmail.com>
2018-12-10 12:55:04 +0000

    Working on rmi server

---
+++ RMI/src/ie/gmit/sw/ds/Setup/CarHireImpl.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.ds.Setup;
+
+import java.rmi.RemoteException;
+
+public class CarHireImpl implements Interface {
+
+	@Override
+	public void createCarHire(String q) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ RMI/src/ie/gmit/sw/ds/Setup/Interface.java
@@ -0,0 +1,14 @@
+package ie.gmit.sw.ds.Setup;
+
+import java.rmi.*;
+import java.util.List;
+
+
+
+
+public interface Interface extends Remote{
+	
+	public void createCarHire(String q) throws RemoteException; // to create a booking
+	
+
+}
